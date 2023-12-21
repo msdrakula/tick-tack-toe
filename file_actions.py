f8ae626d9c24a4f64bd4f29e02374eca9cd43478
Денис Поляков <polyakovdm@is-zone.ru>
2023-12-21 13:51:11 +0300

    Вырузка в файл

---
+++ file_actions.py
@@ -0,0 +1,17 @@
+# file_actions.py
+
+# # Открыть на запись файл example.txt
+# file = open('example.txt', 'w', encoding='utf-8')
+# # Записать в файл строку.
+# file.write('Зевну, укроюсь с головою,\nбудильник заведу на март.\n')
+# # Закрыть файл.
+# file.close() 
+
+# # Открыть файл example.txt на чтение (аргумент 'r').
+# file = open('example.txt', 'r', encoding='utf-8')
+# # Прочитать первые 11 символов из файла и сохранить их в переменную content.
+# content = file.read(12)
+# # Вывести на печать содержимое переменной content.
+# print(content)
+# # Закрыть файл.
+# file.close() 
--- game.py
+++ game.py
@@ -1,6 +1,15 @@
 from gameparts import Board
 from gameparts.exceptions import CellOccupiedError, FieldIndexError
 
+
+def save_result(result):
+    # Открыть файл results.txt в режиме «добавление».
+    # Если нужно явно указать кодировку, добавьте параметр encoding='utf-8'.
+    file = open('results.txt', 'a', encoding='utf-8')
+    # Записать в файл содержимое переменной result.
+    file.write(result + '\n')
+    file.close()
+
 def main():
     # Создать игровое поле - объект класса Board.
     game = Board()
@@ -80,10 +89,20 @@ def main():
 
         # После каждого хода надо делать проверку на победу и на ничью.
         if game.check_win(current_player):
-            print(f'Победили {current_player}!')
+            # Сформировать строку.
+            result = f'Победили {current_player}.'
+            # Вывести строку на печать.
+            print(result)
+            # Добавить строку в файл.
+            save_result(result)
             running = False
         elif game.is_board_full():
-            print('Ничья!')
+            # Сформировать строку.
+            result = 'Ничья!'
+            # Вывести строку на печать.
+            print(result)
+            # Добавить строку в файл.
+            save_result(result)
             running = False
 
         # Тернарный оператор, через кототорый реализована смена игроков.
@@ -91,7 +110,10 @@ def main():
         # иначе — новым значением будет X.
         current_player = 'O' if current_player == 'X' else 'X'
 
-    
+
 
 if __name__ == '__main__':
     main()
+
+
+
